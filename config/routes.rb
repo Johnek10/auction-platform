fe45afc58eafce7e80cf1b7bd7fcc3d0c2ad3eb3
Johnek10 <jansyg10@gmail.com>
2024-01-26 00:17:48 +0100

    added product model and migration with forign key to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   root 'pages#home'
   devise_for :admin, controllers: {
   sessions: 'admin/sessions'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_191652) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_231540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_191652) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.integer "price"
+    t.date "publication_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "login", default: "", null: false
@@ -39,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_191652) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
